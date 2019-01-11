f9ea4adc4211fbd61fc405d899ecc1c34b46b1c2
A-hu <louis0911317399@gmail.com>
2019-01-11 16:37:52 +0800

    Promise doc

---
+++ promise-api.js
@@ -0,0 +1,5 @@
+const p = Promise.resolve({ id: 1 });
+p.then(result => console.log(result));
+
+const pe = Promise.reject(new Error('reason for rejection...'));
+pe.catch(error => console.log(error.message));
